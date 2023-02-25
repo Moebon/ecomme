e12e977ce7faaf921dfcdca179cc902737935120
Moebon <113443610+Moebon@users.noreply.github.com>
2023-02-25 07:35:29 -0800

    Add files via upload

---
+++ ecom/components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact us</h3>
+         <a href="tel:0909090909"><i class="fas fa-phone"></i> +2519999999999</a>
+         <a href="tel:0912345678"><i class="fas fa-phone"></i> +251987777765</a>
+         <a href="mailto:shaikh@gmail.com"><i class="fas fa-envelope"></i> mohiboonebisa@gmail.com</a>
+         <a href="https://www.google.com/maps/place/Addis+Ababa/data=!4m2!3m1!1s0x164b85cef5ab402d:0x8467b6b037a24d49?sa=X&ved=2ahUKEwi9jK6Z26b9AhW6k4kEHWnKAD8Q8gF6BAgIEAI"><i class="fas fa-map-marker-alt"></i> Ethiopia, A/A </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+         <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Moebon Ebisa</span> | all rights reserved!</div>
+
+</footer>
